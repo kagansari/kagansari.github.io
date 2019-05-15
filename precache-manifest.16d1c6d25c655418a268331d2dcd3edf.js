333e8812ae7869573174ef3508e9e80ca2bba63f
Kağan Sarı <kaganalisari@gmail.com>
2019-05-15 11:53:33 +0300

    theme

---
+++ precache-manifest.16d1c6d25c655418a268331d2dcd3edf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d83b3b061cb5239bb739e3c07fd3fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41afa9b31a62a28c2bbd",
+    "url": "/static/js/2.2d931749.chunk.js"
+  },
+  {
+    "revision": "a2fc3453c331deab1cb0",
+    "url": "/static/js/main.ece8ad4f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c229f95bb3df37433c5358f476874655",
+    "url": "/static/media/mock-pp.c229f95b.png"
+  }
+]);
